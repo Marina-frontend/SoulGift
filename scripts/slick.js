132bebe17bec221e75c8dbb80bc6b3f1c9146d59
Marina Kovaleva <sunshining1106@mail.ru>
2023-02-08 12:33:00 +0300

    Add section Main

---
+++ scripts/slick.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.slider').slick({
+        arrows: false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+});
