bdf0c01e682abea7aaf619b2d37415108f7d737b
Marina Kovaleva <sunshining1106@mail.ru>
2023-02-16 18:57:36 +0300

    Add popup for the product 1

--- scripts/slick.js
+++ scripts/slick.js
@@ -7,4 +7,18 @@ $(document).ready(function(){
 		speed: 2000,
 		autoplaySpeed:3000,
 	});
+	$('.popup-product__slider_big').slick({
+        arrows: false,
+		dots:false,
+		autoplay: false,
+		fade: true,
+		asNavFor: ".popup-product__slider_small",
+	});
+	$('.popup-product__slider_small').slick({
+        arrows: false,
+		dots:false,
+		slidesToShow: 3,
+		autoplay: false,
+		asNavFor: ".popup-product__slider_big",
+	});
 });
