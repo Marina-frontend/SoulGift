42aadc9bdd2a14ab46aa9a43f3c9ca8bf8b2a835
Marina Kovaleva <sunshining1106@mail.ru>
2023-02-20 18:04:51 +0300

    Made adaptive for product's popup, fix its sliders, made dropdown

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,58 @@ $(document).ready(function(){
     });
 });
 
+class Dropdown {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector)
+        this.items = options.items
+        
+        this.$el.querySelector('.dropdown__label').textContent = this.items[0].label
+    
+        this.$el.addEventListener('click', event => {
+            if (event.target.classList.contains('dropdown__label')) {
+                if (this.$el.classList.contains('open')) {
+                    this.close()
+                } else {
+                    this.open()
+                }
+            } else {
+                if (event.target.tagName.toLowerCase() === 'li') {
+                    this.select(event.target.dataset.id)
+                }
+            }
+        })
 
+        const itemsHTML = this.items.map(i => {
+            return `<li data-id="${i.id}">${i.label}</li>`
+        }).join(' ')
+
+        this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin',
+        itemsHTML)
+    }
+
+    select(id) {
+        const item = this.items.find(i => i.id === id)
+        this.$el.querySelector('.dropdown__label').textContent = item.label
+        this.close()
+    }
+
+    open() {
+        this.$el.classList.add('open')
+    }
+
+    close() {
+        this.$el.classList.remove('open')
+    }
+}
+
+
+const dropdown01 = new Dropdown('#dropdown01', {
+    items: [
+        {label: '30 минут', id: 'h1'},
+        {label: '1 час', id: 'h2'},
+        {label: '1,5 часа', id: 'h15'},
+    ]
+})
 
 
 
